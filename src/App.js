7e8a6ca97875a404310e741ac74f44091f777ece
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-16 11:23:32 +0200

    Add Scream

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 // Components
 import Navbar from './components/Navbar';
 
@@ -10,21 +11,40 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#6573c3',
+			main: '#3f51b5',
+			dark: '#2c387e',
+			contrastText: '#fff'
+		},
+		secondary: {
+			light: '#ffee33',
+			main: '#ffea00',
+			dark: '#b2a300',
+			contrastText: '#fff'
+		}
+	}
+});
+
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
-				<Router>
-        <Navbar />
-          <div className="container" >
-					<Switch>
-						<Route exact path='/' component={home} />
-						<Route exact path='/login' component={login} />
-						<Route exact path='/signup' component={signup} />
-					</Switch>
-          </div>
-				</Router>
-			</div>
+			<ThemeProvider theme={theme}>
+				<div className='App'>
+					<Router>
+						<Navbar />
+						<div className='container'>
+							<Switch>
+								<Route exact path='/' component={home} />
+								<Route exact path='/login' component={login} />
+								<Route exact path='/signup' component={signup} />
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</ThemeProvider>
 		);
 	}
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+// Components
+import Scream from '../components/Scream';
 
 class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>home Page</h1>
-            </div>
-        )
-    }
+	state = {
+		screams: null
+	};
+
+	componentDidMount() {
+		axios
+			.get(' /screams')
+			.then((res) => {
+				this.setState({
+					screams: res.data
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
+	render() {
+		let recentScreamsMarkup = this.state.screams ? (
+			this.state.screams.map((scream) => <Scream scream={scream} key={scream.screamId} />)) : <p>Loading...</p>;
+
+		return (
+			<Grid container spacing={10}>
+				<Grid item sm={8} sx={12}>
+					{recentScreamsMarkup}
+				</Grid>
+				<Grid item sm={4} sx={12}>
+					<p>Progile...</p>
+				</Grid>
+			</Grid>
+		);
+	}
 }
 
 export default home;
