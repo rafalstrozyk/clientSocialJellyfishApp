bc3672157cf9b736c71f37420af2f157873ced42
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-21 12:19:54 +0200

    PostScream

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -3,13 +3,17 @@ import {
 	LOADING_DATA,
 	LIKE_SCREAM,
 	UNLIKE_SCREAM,
-	DELETE_SCREAM
+	DELETE_SCREAM,
+	LOADING_UI,
+	SET_ERRORS,
+	CLEAR_ERRORS,
+	POST_SCREAM
 } from '../types';
 import axios from 'axios';
 
 // Get all screams
 export const getScreams = () => (dispatch) => {
-	dispatch({ type:LOADING_DATA });
+	dispatch({ type: LOADING_DATA });
 	axios
 		.get('/screams')
 		.then((res) => {
@@ -19,6 +23,25 @@ export const getScreams = () => (dispatch) => {
 			dispatch({
 				type: SET_SCREAMS,
 				payload: []
+			});
+		});
+};
+// post a scream
+export const postScream = (newScream) => (dispatch) => {
+	dispatch({ type: LOADING_UI });
+	axios
+		.post('/scream', newScream)
+		.then((res) => {
+			dispatch({
+				type: POST_SCREAM,
+				payload: res.data
+			});
+			dispatch({ type: CLEAR_ERRORS });
+		})
+		.catch((err) => {
+			dispatch({
+				type: SET_ERRORS,
+				payload: err.response.data
 			});
 		});
 };
@@ -50,9 +73,14 @@ export const unlikeScream = (screamId) => (dispatch) => {
 };
 
 export const deleteScream = (screamId) => (dispatch) => {
-	axios.delete(`/scream/${screamId}`)
+	axios
+		.delete(`/scream/${screamId}`)
 		.then(() => {
-			dispatch({type: DELETE_SCREAM, payload: screamId})
+			dispatch({ type: DELETE_SCREAM, payload: screamId });
 		})
-		.catch(err => console.log(err));
-}
+		.catch((err) => console.log(err));
+};
+
+export const clearErrors = () => (dispatch) => {
+	dispatch({ type: CLEAR_ERRORS });
+  };
--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -3,7 +3,8 @@ import {
 	LIKE_SCREAM,
 	UNLIKE_SCREAM,
 	LOADING_DATA,
-	DELETE_SCREAM
+	DELETE_SCREAM,
+	POST_SCREAM
 } from '../types';
 
 const initialState = {
@@ -43,6 +44,14 @@ export default function (state = initialState, action) {
 			return {
 				...state
 			};
+		case POST_SCREAM: 
+			return {
+				...state,
+				screams: [
+					action.payload,
+					...state.screams
+				]
+			} 
 		default:
 			return state;
 	}
