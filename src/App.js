4bcb0f957b2fa28fb5857c08fa1a68428c85d25e
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-23 12:17:23 +0200

    Static profile

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ import AuthRoute from './util/AuthRoute';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import user from './pages/user';
+
+
 import axios from 'axios';
 
 const theme = createMuiTheme(themeFile);
@@ -49,6 +52,7 @@ class App extends Component {
 								<Route exact path='/' component={home} />
 								<AuthRoute exact path='/login' component={login} />
 								<AuthRoute exact path='/signup' component={signup} />
+								<Route exact path="/users/:handle" component={user} />
 							</Switch>
 						</div>
 					</Router>
